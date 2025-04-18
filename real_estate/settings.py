d2648fcb99671242d520d91661fe98e25348dffe
Eeshu <eeshuyadav123@gmail.com>
2025-04-18 13:15:05 +0000

    added the apps directory

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -5,18 +5,21 @@ import environ
 env = environ.Env(DEBUG=(bool, False))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(BASE_DIR / ".env")
+
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^f$rdhbh3kd1%3-=(n=*c7yi$9u&*)37(6hai@y06=3u=*sqfa'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
